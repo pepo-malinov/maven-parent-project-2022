c6d2eae48752de843bccf2ec33829342484ac558
pepo-malinov <pepo.malinov@gmail.com>
2022-05-08 11:03:00 +0300

    init project

---
+++ first-child/src/test/java/uni/pu/fmi/st/CategoryTests.java
@@ -0,0 +1,25 @@
+package uni.pu.fmi.st;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class CategoryTests {
+
+	@Test
+	@Category(FatsTest.class)
+	public void testFastTest() {
+		System.out.println("--------------------FatsTest--------------");
+	}
+
+	@Test
+	@Category(SlowTest.class)
+	public void testSlowTest() {
+		System.out.println("--------------------SlowTest--------------");
+	}
+	
+	@Test
+	@Category({SlowTest.class, FatsTest.class})
+	public void testTest() {
+		System.out.println("--------------------testTest--------------");
+	}
+}
